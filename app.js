2d707795ced61ab2fcccfddabc22e214eb551752
samuel21695 <samuel21695@gmail.com>
2023-10-06 10:01:33 +0900

    function surbur

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log('hi')
+function surbur(callback) {
+  let request = {
+    insa : 'hello'
+  };
+  let response = {
+    insa : 'bye'
+  };
+  return callback(request, response);
+}
+
+surbur(function(request, response){
+  console.log(request.insa);
+  console.log(response.insa);
+})
